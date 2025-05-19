49db7690f3299008a18c1d3350c08a88c4282244
fanarkt08 <fanarkt08@gmail.com>
2025-05-19 11:47:51 +0200

    Activity 5

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer>
+        Copyright Mexican Food 2024
+      </footer>
+    );
+  }
+  
+  export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
           src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" 
         />
         <nav>
-          <a href="index.html">Accueil</a>
+        <a href="/">Accueil</a>
         </nav>
       </header>
     );

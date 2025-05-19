d39092412d49bc22bfca35ab0f31c367f2588763
fanarkt08 <fanarkt08@gmail.com>
2025-05-19 11:42:58 +0200

    Activity 4

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+      <header>
+        <img 
+          src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" 
+        />
+        <nav>
+          <a href="index.html">Accueil</a>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Header;
+  

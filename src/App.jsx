43871af6711a0ad71d70e0c6f9382fe39bc83dbe
fanarkt08 <fanarkt08@gmail.com>
2025-05-20 11:45:35 +0200

    Activity 11

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,26 @@ const Dishes = [
     image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
     name: "Tacos à l’unité",
     price: 3,
-    isNew: true
+    isNew: true,
+    stock: 12,
   },
   {
     image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
     name: "Enchiladas",
     price: 12,
-    isNew: false
+    isNew: false,
+    stock: 0,
   },
   {
     image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
     name: "Mole poblano",
     price: 15,
-    isNew: false
+    isNew: false,
+    stock: 5,
   }
 ];
+
+const filteredDishes = Dishes.filter(Dish => Dish.stock > 0)
 
 function App() {
   return (
@@ -31,7 +36,7 @@ function App() {
       <Header />
       <Container as="main" className="my-4">
         <Row>
-          {Dishes.map((dish, index) => (
+          {filteredDishes.map((dish, index) => (
             <Col md={4} key={index}>
               <Dish
                 image={dish.image}

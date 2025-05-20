fdd4810038a3c534ac6d1d756abe8a8c8b584fe9
fanarkt08 <fanarkt08@gmail.com>
2025-05-20 11:03:12 +0200

    Activity 10

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,45 @@ import Dish from './components/Dish';
 import { Container, Row, Col } from 'react-bootstrap';
 import './assets/styles/App.scss';
 
+const Dishes = [
+  {
+    image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+    name: "Tacos à l’unité",
+    price: 3,
+    isNew: true
+  },
+  {
+    image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+    name: "Enchiladas",
+    price: 12,
+    isNew: false
+  },
+  {
+    image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+    name: "Mole poblano",
+    price: 15,
+    isNew: false
+  }
+];
+
 function App() {
   return (
     <>
       <Header />
-        <Container as="main" className="my-4">
-          <Row>
-            <Col md={4}>
+      <Container as="main" className="my-4">
+        <Row>
+          {Dishes.map((dish, index) => (
+            <Col md={4} key={index}>
               <Dish
-                image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-                name="Tacos à l’unité"
-                price={3}
-                isNew={true}
+                image={dish.image}
+                name={dish.name}
+                price={dish.price}
+                isNew={dish.isNew}
               />
             </Col>
-            <Col md={4}>
-              <Dish
-                image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-                name="Enchiladas"
-                price={12}
-                isNew={false}
-              />
-            </Col>
-            <Col md={4}>
-              <Dish
-                image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-                name="Mole poblano"
-                price={15}
-                isNew={false}
-              />
-            </Col>
-          </Row>
-        </Container>
+          ))}
+        </Row>
+      </Container>
       <Footer />
     </>
   );
